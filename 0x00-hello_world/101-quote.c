9939794e7cb299e00cd999378f9854c76e093caa
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-09 16:33:45 +0300

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - Where execution of the programm starts
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
