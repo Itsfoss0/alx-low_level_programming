ee75ef1f8065421b7ed37807535996a867804b52
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-13 19:08:39 +0300

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*add - add two integer number and return the value
+*@a : - the first argument of the add function
+*@b : - The second argument of the add function
+* Return: void
+*/
+int add(int a, int b)
+{
+int result;
+
+result = a + b;
+return (result);
+}
