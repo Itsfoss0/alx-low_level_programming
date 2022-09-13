61aa619c759eb9cb956e0648dd563277f6630613
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-13 19:23:20 +0300

    Added 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  *print_to_98 - function to print all natural to 98
- *@n :- The number from which the function starts 
- *Return - void (no return value from this function 
+ *@n :- The number from which the function starts
+ *Return - void (no return value from this function
  */
 void print_to_98(int n)
 {
-  while ( n <= 98)
-    {
-      _putchar(n + "44" + '0');
-      n++;
-    }
-  return;
+while (n <= 98)
+{
+_putchar(n + "44" + '0');
+n++;
+}
+return;
 }
