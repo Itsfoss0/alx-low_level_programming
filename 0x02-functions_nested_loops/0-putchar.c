79c54055e0bf6a4df5fe332d7097c52bf4a1e9aa
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-13 13:44:11 +0300

    Added 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description - Print putchar with a newline character at the end
- * Return: Always 0 (Success)
+ * Description: prints '_putchar\n'
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-char str[] = "_putchar";
+char str[] = "_putchar"
 int i = 0;
 
 while (str[i] != '\0')
-{
-char c  = str[i];
-_putchar(c);
-}
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
 return (0);
 }
