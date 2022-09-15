540e18269196be29d2090f1ce61043be581cbdec
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-15 10:23:22 +0300

    Added print_remaining_days( ) function prototype in the main.h header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+print_remaining_days(int month, int day,int year);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 #endif /* MAIN_H */
