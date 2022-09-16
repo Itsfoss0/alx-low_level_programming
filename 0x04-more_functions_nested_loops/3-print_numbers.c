d7eb5cc7e0f99a6698593e53472775234f0b6e9b
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-16 16:54:22 +0300

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - print numbers from 0-9
+ *return: 0 (sucess) or 1(failure)
+ */
+
+void print_numbers(void)
+{
+for (int c = 48; c <= 58; c++)
+{
+_putchar(c);
+}
+}
