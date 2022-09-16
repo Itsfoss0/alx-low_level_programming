59884b36878121782046b5685f2df6f39f873e3c
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-16 15:10:55 +0300

    Added 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,9 +9,10 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 &&  year % 400 == 0) || (year % 4 == 0))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month > 2 && day >= 60)
 		{
