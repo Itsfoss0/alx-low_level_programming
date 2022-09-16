adf9a7f85630637a6250cb8ebc28435047557f34
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-16 15:17:11 +0300

    Added int convert_day( ) function prototype in the main.h header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 print_remaining_days(int month, int day,int year);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
 #endif /* MAIN_H */
