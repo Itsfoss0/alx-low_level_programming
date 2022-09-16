41febb6f449c5253df4a5e91289898a82c64922c
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-16 15:18:45 +0300

    Added main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-print_remaining_days(int month, int day,int year);
+print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
