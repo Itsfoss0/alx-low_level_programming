3dccaf6c49b0779e1de647cb4d7e4bbf3d1a46ea
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-19 22:12:42 +0300

    Added 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _atoi - convert a number in a string to an integer
+ * @s: String to be converted
+ * Return: converted integer OR 0 if none
+ */
+
+int _atoi(char *s)
+{
+	int sign_cache = 1;
+	int result = 0;
+
+	while (s[0] != '\0')
+	{
+		if (s[0] == '-')
+			sign_cache *= -1;
+		else if (s[0] >= '0' && s[0] <= '9')
+			result = (result * 10) + (s[0] - '0') * sign_cache;
+		else if (result)
+			break;
+
+		s++;
+	}
+
+	return (result);
+}
