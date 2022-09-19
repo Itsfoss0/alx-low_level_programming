a05c8591a76537198694a5058871e981da655738
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-19 18:53:48 +0300

    Added 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,29 +5,16 @@
  *Return: void
  */
 
-void rev_string(char *s)
+
+void print_rev(char *s)
 {
-	int i = 0, j, right;
+	int i = 0;
 
 	while (s[i])
 		i++;
 
-	j = i / 2;
-	right = 0;
+	while (i--)
+		_putchar(s[i]);
 
-	while (right != j)
-	{
-		char temp = s[right];
-		int left = i - right - 1;
-
-		s[right] = s[left];
-		s[left] = temp;
-
-		right++;
-	}
-
-}
-int main(void)
-{
-return (0);
+	_putchar('\n');
 }
