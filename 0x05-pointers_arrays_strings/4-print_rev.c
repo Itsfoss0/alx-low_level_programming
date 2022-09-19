247a361ce5f83e007c8f39fa3965b465866ac66d
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-19 18:26:47 +0300

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_rev - print a string in reverse
+ *@s: pointer to the string to be printed
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	char *t = s;
+	char n[1000];
+	short c = 0;
+
+	while (*s != '\0')
+	{
+		n[c] = *s;
+		s++;
+		c++;
+	}
+	c = 0;
+
+	while (s > t)
+	{
+		s--;
+		*s = n[c];
+		c++;
+	}
+}
