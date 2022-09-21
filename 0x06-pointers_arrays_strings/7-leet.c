a1106183584c498406eefe2f561ca36b52aac3fd
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-21 22:03:07 +0300

    Added 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encode
+ * @s: pointer to char params
+ * Return: *s
+ */
+
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char l[] = "ol_ea__t";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j] || s[i] == (l[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
+}
