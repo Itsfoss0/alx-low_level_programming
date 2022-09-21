bc473ef2dc6726b1678f61cdccdd636fb2427516
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-21 21:03:13 +0300

    Added 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * reverse_array - reverse
+ * @a : pointer to int params
+ * @n : pointer to int params
+ * Return: no
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+	int tmp;
+
+	i = 0;
+	j = n - 1;
+
+	while (i < j)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+		j--;
+	}
+}
