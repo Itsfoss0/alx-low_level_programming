481293afa1fb2b0723f28465d031b6644e6876eb
Itsfoss0 <akhonyaebenezer@gmail.com>
2022-09-28 17:48:44 +0300

    Added 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (s != "\0")
-{
-_print_rev_recursion(s + 1);
-_putcahr(*s);
-}
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
