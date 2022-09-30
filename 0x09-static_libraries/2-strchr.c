b6fe48d82749d1190c515ebe3c24e37b9a3619f4
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-30 23:56:06 +0300

    Added 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: If c is found - a pointer to the first occurence.
+ * If c is not found - NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
+}
