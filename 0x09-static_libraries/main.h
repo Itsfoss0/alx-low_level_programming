d790c880e972e3a3ff92160731cacecfba9e290a
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-30 22:25:44 +0300

    Added the last 3 function prototypes to the main.h header file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -15,3 +15,6 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
