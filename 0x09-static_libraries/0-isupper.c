7391a6a8be5729c4e176d8f4d836d0be008ec6c3
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-30 22:46:02 +0300

    Added 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
