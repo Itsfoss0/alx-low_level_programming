f7134f87b08e4bf21f97f22ebf4863588996f945
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-01 00:03:53 +0300

    Added 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return: If str1 < str2, the negative difference of
+ * the first unmatched characters.
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference of
+ *         wthe first unmatched characters.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
