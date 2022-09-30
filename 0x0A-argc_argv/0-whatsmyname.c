6d393c1935876a9342b555bd429f4c4f8e89b9b8
Itsfoss0 <akhonyaebenezer@gmail.com>
2022-10-01 01:24:38 +0300

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - print the name of argv[0]
+ * @argv: pointer to the string args
+ * @argc: argument count
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
