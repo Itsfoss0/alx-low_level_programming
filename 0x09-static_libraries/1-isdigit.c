05b1e7c491e173f6e5f63e261e80a5d0234ac9d8
Ebenezer <akhonyaebenezer@gmail.com>
2022-09-30 23:47:20 +0300

    Added 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
