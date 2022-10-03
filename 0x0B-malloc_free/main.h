80721974b352b222012f2cce245a6afd58de4f72
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-03 21:28:21 +0300

    Added int **alloc_grid(int width, int height) function prototype in the main.h header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,5 @@ int _putchar(char);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int _strlen(char *s)
+int _strlen(char *s);
 int **alloc_grid(int width, int height);
