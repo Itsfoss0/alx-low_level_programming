e66dfbdb4fcdd65d4a3e13dfa1b19cbaaae13a89
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-13 00:01:33 +0300

    Added int _strlen(char *s) function protototype in the dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,4 +26,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
