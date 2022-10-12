bf87901979a8946c4201300c129e73fdb2f61664
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-12 23:32:22 +0300

    Added dog.h header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*struct dog - structure representing a dog
+*@name: the dogs' name
+*@age: the dog's age
+*@owner: owner's name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
