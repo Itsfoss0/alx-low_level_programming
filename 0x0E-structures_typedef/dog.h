3412b60fafdacf03d450fa9611cc1b15c1d49cb9
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-13 00:03:00 +0300

    Added void free_dog(dog_t *d) function prototype in the dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,5 +27,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+void free_dog(dog_t *d);
 
 #endif
