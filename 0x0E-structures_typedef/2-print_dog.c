47cf74a6b040a01f922dcd0935ae95207d4214b3
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-12 23:53:35 +0300

    Added 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,16 +15,16 @@ return;
 if ((*d).name == NULL)
 printf("nil\n");
 else
-printf("Name: %s\n", d.name);
+printf("Name: %s\n", d->name);
 
 if ((*d).age < 0)
 printf("nil\n");
 else
-printf("Age: %f\n", d.age);
+printf("Age: %f\n", d->age);
 
 if ((*d).owner == NULL)
 printf("nil\n");
 else
-printf("Owner: %s\n", d.owner);
+printf("Owner: %s\n", d->owner);
 
 }
