848b5f1cc0ed5700a9f9d8f53340c3613e882cf2
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-13 00:00:33 +0300

    Added char *_strcpy(char *dest, char *src) function prototype to the dog.h header file"

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,5 +25,5 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
+char *_strcpy(char *dest, char *src);
 #endif
