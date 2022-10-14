d363c526e648484bbbb668f7240a0881b9c4c2b8
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-14 21:16:10 +0300

    Added void print_strings(const char *separator, const unsigned int n, ...) function prototype in the variadic_functions.h header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #include <stdio.h>
 #include <stdarg.h>
