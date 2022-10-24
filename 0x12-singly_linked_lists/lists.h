f04da411eb3de8b83e1e89d05e730c5cafcb3df2
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-24 13:54:52 +0300

    Added list_s structure in the lists.h header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,2 +1,22 @@
+/** function prototypes here**/
+
 int _putchar(char);
 size_t print_list(const list_t *h);
+
+
+/** structures here **/
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ *              for the ALX project
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
