fba2d47916abf446333ddcf36f90d3850886a4d8
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-24 15:01:55 +0300

    Added 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,14 +4,13 @@
  *            a linked list_t list.
  * @h: The linked list_t list.
  * @elements: a counter var for size_t (nodes)
- *
- * Return: The number of elements in h.
+ * * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		elements++;
 		h = h->next;
