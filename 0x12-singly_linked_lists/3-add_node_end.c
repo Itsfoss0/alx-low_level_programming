a00f038fb3da5400ab4dbe07f18488c7da9e2a95
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-25 11:37:58 +0300

    Added 3-add_node.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,7 +5,6 @@
  * @str: Address of the string to insert into the new node.
  * Return: Address of the new node.
  **/
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp, *temp2;
