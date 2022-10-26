2855c7bc6ccd2d31974ae5720ae85a72882b7964
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-26 18:25:34 +0300

    Fixed the typedef error in the lists.h header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,11 +3,6 @@
 #include <string.h>
 #include <stdio.h>
 
-/** function prototypes here **/
-
-int _putchar(char);
-size_t print_listint(const listint_t *h);
-
 /** structures here **/
 /**
  * struct listint_s - singly linked list
@@ -20,5 +15,11 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/** function prototypes here **/
+
+int _putchar(char);
+size_t print_listint(const listint_t *h);
+
 
 #endif
