bfaaf8e149e40fc8ec749e117fd877768d718692
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-26 19:41:13 +0300

    Added size_t free_listint_safe(listint_t **h) function prototype in the lists.h header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,5 +32,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
 #endif
