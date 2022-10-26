2b885d54bc4d1b579bb35b08088119c31522f73e
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-26 18:18:43 +0300

    Added listint_s strucuture in the lists.h header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,3 +2,16 @@
 
 int _putchar(char);
 size_t print_listint(const listint_t *h);
+
+/** structures here **/
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
