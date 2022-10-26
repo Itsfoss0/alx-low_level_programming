b92fa0c524e571c2d2a2d2831e561c043b84b369
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-26 18:46:05 +0300

    Added listint_t *add_nodeint_end(listint_t **head, const int n) function prototype in the lists.h header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,5 +22,6 @@ int _putchar(char);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif
