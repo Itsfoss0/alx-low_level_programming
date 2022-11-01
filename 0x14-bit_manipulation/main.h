81666c8b438e21da122b3c8bb28ea13ae10b1719
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-01 14:44:36 +0300

    Added int set_bit(unsigned long int *n, unsigned int index) function prototypes in the main.h header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
-
-
 /** Function prototypes here **/
 int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
