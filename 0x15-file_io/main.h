5713807e4e8fa1c047f9bcdd0d81a94ae9b156de
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-02 14:56:45 +0300

    Added int create_file(const char *filename, char *text_content) function prototype in the main.h header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,7 @@
 
 /** function prototypes here **/
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 int _putchar(char);
 
 #endif
