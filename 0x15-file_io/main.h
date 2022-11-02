e425891052391c869a8d16092f96d9f27e0b64eb
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-02 15:29:15 +0300

    Added main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <elf.h>
 /** function prototypes here **/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
