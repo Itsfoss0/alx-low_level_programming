9b3fd701de40dd89713f1314774c8da2a27e5c3c
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-02 14:42:39 +0300

    Added standard header files in the main.h header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/** standard header files **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /** function prototypes here **/
 ssize_t read_textfile(const char *filename, size_t letters);
