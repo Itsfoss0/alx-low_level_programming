5f349794b66ac66189f37f83a03c9805ef535c27
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-02 15:27:55 +0300

    Added main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,6 +15,14 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char);
-
+void elf_validation(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+unsigned int lit_to_big_endian(unsigned int x);
+void print_entry(unsigned int e_type, unsigned char *e_ident);
 
 #endif
