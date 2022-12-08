11de12388e9adcf86591a06d8f099cbdbdbb00f0
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-08 20:24:03 +0300

    Added int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) function prototype in the lists.h header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,4 +27,5 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 #endif
