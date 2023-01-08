e77c6563113b84a15e3ff0ccd15c3768f4758fe8
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-08 20:36:29 +0300

    Added header files

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,6 @@ hash_node_t **array;
 /*function prototypes*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif
