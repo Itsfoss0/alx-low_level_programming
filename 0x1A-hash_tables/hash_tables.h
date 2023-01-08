faba097c8e13b4e3eed1a191fa56bc4064374d9c
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-08 21:06:08 +0300

    Added Header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -45,5 +45,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *create_new_node (const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key)
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
