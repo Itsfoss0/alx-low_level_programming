3a879372bfd7c1ece69144182f6e02f520995119
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-08 21:03:52 +0300

    Added 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,19 +7,26 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int idx;
 	hash_node_t *node;
-	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
+	idx = key_index((unsigned char *) key, ht->size);
+
+	if (ht->array[idx] == NULL || ht->array[idx] == 0)
 		return (NULL);
 
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
+	if (strcmp(ht->array[idx]->key, key) == 0)
+		return (ht->array[idx]->value);
+	node = ht->array[idx];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
 		node = node->next;
+	}
 
-	return ((node == NULL) ? NULL : node->value);
+	return (NULL);
 }
