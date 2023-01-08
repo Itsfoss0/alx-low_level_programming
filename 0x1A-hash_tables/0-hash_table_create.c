cf0518bac0201fbbc8ddff871a88c354e9efd8b5
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-08 20:23:15 +0300

    Added 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int j = 0;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	for (j; j < size; j++)
+		hash_table->array[j] = NULL;
+	return (hash_table);
+}
