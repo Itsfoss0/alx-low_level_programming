35c5bb205fdd47cbf2c55d0259a2ca09f28505ce
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-08 20:59:47 +0300

    Added 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - return a value associated with a key.
+ * @ht: the hash table to search
+ * @key: the search key
+ * Return: value['key'] if found, NULL if else
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
