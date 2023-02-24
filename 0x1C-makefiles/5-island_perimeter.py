de8144f57d898d4e4f673a7b932b1f42a52cd137
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-24 21:45:22 +0300

    Added 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,7 +4,7 @@ Grid perimeter calculator module
 """
 
 
-def island_perimeter(grid):
+def island_perimeter(grid) -> int:
     """"Culculate the perimter of a grid
     Args:
         grid (list): A list of lists
@@ -22,4 +22,4 @@ def island_perimeter(grid):
                     edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
-    return size * 4 - edges * 2
+    return (size * 4 - edges * 2)
