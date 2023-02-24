7ee3e7d72fa4c2cd1b4d83f91a1829b7916838c9
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-24 21:43:24 +0300

    Added 5-island_perimter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Grid perimeter calculator module
+"""
+
+
+def island_perimeter(grid):
+    """"Culculate the perimter of a grid
+    Args:
+        grid (list): A list of lists
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
